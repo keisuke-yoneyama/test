957bc9942809ed8e19f3225de7e5ce6dbc47b2c7
keisuke-yoneyama <charity.goodinvestment@gmail.com>
2018-07-13 21:40:19 +0900

    Java7限終了

---
+++ lessonproject01/src/lessonproject01/Rensyu01.java
@@ -0,0 +1,16 @@
+package lessonproject01;
+
+public class Rensyu01 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+
+		int a = 5;
+
+		if(a%2==0){
+			System.out.println("‹ô”");
+		}else{
+			System.out.println("Šï”");
+		}
+	}
+}
